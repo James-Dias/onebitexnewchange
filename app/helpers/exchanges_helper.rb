41f5f7c5f8ea3da0256a6947f83b5a2cb401b7f1
James Dias <jamesdias0@gmail.com>
2019-11-03 16:00:56 -0300

    Change currency and cryptocurrency conversion API; Change style

--- app/helpers/exchanges_helper.rb
+++ app/helpers/exchanges_helper.rb
@@ -2,10 +2,16 @@ module ExchangesHelper
 
   def currency_list
     [
-      'AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP',
-      'HKD', 'HRK', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KRW', 'MXN',
-      'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD',
-      'THB', 'TRY', 'USD', 'ZAR'
+      'AED','AFN','ALL','AMD','ANG','AOA','ARS','AUD','AWG','AZN','BAM','BBD','BDT','BGN','BHD','BIF','BMD','BND',
+      'BOB','BRL','BSD','BTC','BTN','BWP','BYN','BYR','BZD','CAD','CDF','CHF','CLF','CLP','CNY','COP','CRC','CUC',
+      'CUP','CVE','CZK','DJF','DKK','DOP','DZD','EGP','ERN','ETB','EUR','FJD','FKP','GBP','GEL','GGP','GHS','GIP',
+      'GMD','GNF','GTQ','GYD','HKD','HNL','HRK','HTG','HUF','IDR','ILS','IMP','INR','IQD','IRR','ISK','JEP','JMD',
+      'JOD','JPY','KES','KGS','KHR','KMF','KPW','KRW','KWD','KYD','KZT','LAK','LBP','LKR','LRD','LSL','LVL','LYD',
+      'MAD','MDL','MGA','MKD','MMK','MNT','MOP','MRO','MUR','MVR','MWK','MXN','MYR','MZN','NAD','NGN','NIO','NOK',
+      'NPR','NZD','OMR','PAB','PEN','PGK','PHP','PKR','PLN','PYG','QAR','RON','RSD','RUB','RWF','SAR','SBD','SCR',
+      'SDG','SEK','SGD','SHP','SLL','SOS','SRD','STD','SVC','SYP','SZL','THB','TJS','TMT','TND','TOP','TRY','TTD',
+      'TWD','TZS','UAH','UGX','USD','UYU','UZS','VEF','VND','VUV','WST','XAF','XAG','XCD','XDR','XOF','XPF','YER',
+      'ZAR','ZMK','ZMW','ZWL'
     ]
   end
 end
--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -8,16 +8,18 @@ class ExchangeService
     @amount = amount.to_f
   end
 
-
   def perform
     begin
       exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
       exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
-      url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
-      res = RestClient.get url
-      value = JSON.parse(res.body)['currency'][0]['value'].to_f
 
-      value * @amount
+      url = "#{exchange_api_url}?q=#{@source_currency}_#{@target_currency}&compact=ultra&apiKey=#{exchange_api_key}"
+      res = RestClient.get url
+      value = JSON.parse(res.body)["#{@source_currency}_#{@target_currency}"].to_f
+
+      result =  @amount * value
+
+      ActiveSupport::NumberHelper.number_to_currency(result, precision: 10, unit: "", strip_insignificant_zeros: true, separator: ",", delimiter: ".")
     rescue RestClient::ExceptionWithResponse => e
       e.response
     end
